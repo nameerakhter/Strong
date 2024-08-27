d331a0634eacd32940d0d5499c3429904e29c358
nameerakhter <akhtarnameer@gmail.com>
2024-08-27 21:54:15 +0530

    feat: update layout

--- src/components/Services.tsx
+++ src/components/Services.tsx
@@ -3,10 +3,19 @@ import ServiceCard from './cards/ServiceCard'
 
 const Services = () => {
   return (
-    <div className='mt-[2rem] px-[2rem]'>
-      <div className='flex items-center gap-3'><h1>OUR</h1><h1 className='text-[#c60811]'>Services</h1></div>
-      <ServiceCard />
+    <div className='flex flex-col items-center jsutify-center mt-[2rem] px-[2rem]'>
+      <div className='text-4xl p-[2rem] flex items-center jsutify-center gap-3'>
+        <h1 className=''>Our</h1>
+        <h1 className='text-[#c60811]'>Services</h1>
+      </div>
+      <div className='flex items-center justify-center gap-[2rem]'>
+        <section className='w-1/4'><ServiceCard /></section>
+        <section className='w-1/4'><ServiceCard /></section>
+        <section className='w-1/4'><ServiceCard /></section>
+        <section className='w-1/4'><ServiceCard /></section>
 
+
+      </div>
     </div>
   )
 }
--- src/components/cards/ServiceCard.tsx
+++ src/components/cards/ServiceCard.tsx
@@ -1,12 +1,20 @@
+import Image from 'next/image'
 import React from 'react'
 
 const ServiceCard = () => {
   return (
-    <div>
-      <div>
-        <h1>Lose Weight</h1>
-        <p>Click to join our Weight loss Programs</p>
-        <p>Acheive Sustainaable wieght loss with your....</p>
+    <div className='bg-red-500 rounded-xl w-[20rem]'>
+      <div className=' flex items-center jsutify-center'>
+        <div>
+          <h1>Lose Weight</h1>
+          <p>Click to join our Weight loss Programs</p>
+          <p>Acheive Sustainaable wieght loss with your....</p>
+
+        </div>
+        <div className='blur-2xl bg-[#5e0c0f] '>
+          <Image src="" alt='' />
+        </div>
+
       </div>
     </div>
   )
