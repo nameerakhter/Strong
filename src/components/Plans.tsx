d6cb6384664f853ee83b9941e9a02d980dab6be4
nameerakhter <akhtarnameer@gmail.com>
2024-08-29 09:59:59 +0530

    updated accerternity ui cards

--- src/components/Plans.tsx
+++ src/components/Plans.tsx
@@ -13,13 +13,14 @@ const Plans = () => {
           <h1 className='font-extrabold'>Our</h1>
           <h1 className='text-[#c60811] font-extrabold'>Services</h1>
         </div>
+        <p>Select the plan that suits your fitness foals and let our expert coaches huide you every step of the way</p>
         <div className='relative flex items-center justify-center gap-[2rem]'>
 
-          {/* <section className='w-1/4'><PricingCard h1='Lose Weight' p1='Click to join our Weight loss Programs' p2='Achieve sustainable weight loss with our customized programs, designed to help you burn fat and build a healthier, leaner body. Start your journey to a fitter you today' imgSrc="/assets/Trainers/trainer11.png" /></section> */}
-          {/* <section className='w-1/4'><PricingCard h1='Building Muscle' p1='Click to join our muscle building plans' p2='Develop strength and define your muscles with tailored programs designed to help you gain lean mass efficiently. click on the button below and start your journey right now. don’t miss this cahnce.' imgSrc='/assets/Trainers/trainer2.png' /></section> */}
-          {/* <section className='w-1/4'><PricingCard h1='Training in Home' p1='Click to see our ultimate home plans' p2='Stay fit and strong with our effective home workout plans, requiring minimal equipment. You Can Have Access to a lot of plans by just clicking on learn more!' imgSrc='/assets/Trainers/trainer3.png' /></section> */}
-          {/* <section className='w-1/4'><PricingCard h1='Gym Plan' p1='Click, Enter Your Details, Get Your plan!' p2='Maximize your gym sessions with structured plans that guide you towards your fitness goals.' imgSrc='/assets/Trainers/trainer4.png' /></section> */}
-          {/**/}
+          <section className='w-1/4'><PricingCard /></section>
+          <section className='w-1/4'><PricingCard /></section>
+          <section className='w-1/4'><PricingCard /></section>
+          <section className='w-1/4'><PricingCard /></section>
+
 
         </div>
       </div>
--- src/components/cards/PricingCard.tsx
+++ src/components/cards/PricingCard.tsx
@@ -1,10 +1,34 @@
+"use client";
 import React from 'react'
+import { Button } from '../ui/moving-border'
+import { CardSpotlight } from '../ui/card-spotlight';
 
 const PricingCard = () => {
   return (
-    <div>
+    <CardSpotlight>
+      <div className='flex flex-col items-center justify-center bg-[#1D1D1D]' >
+        <p className='text-sm text-[#cd4e17] pb-[.5rem] '>Package</p>
+        <h1 className='text-4xl font-extrabold p-[1rem]'>ProPlan</h1>
+        <p>Description</p>
+        <p>Our Pro Plan offers advanced workouts and personalized nutrition coaching to help you reach your goals faster. Sign Up Right Now!</p>
+        <p>Features</p>
+        <div>
+          <li>Access to all of our Exercise Videos</li>
+          <li>Progress Tracking</li>
+          <li>Supportive Online Community</li>
+          <li>Advanced personalized workout Plans</li>
+          <li>Comprehensive nutrition coaching</li>
+          <li>Access to Advanced Analysis</li>
+          <li>Body Composition Analysis</li>
+        </div>
+        <div>
+          <h2>99$</h2>
+          <p>/USDT</p>
+        </div>
+        <Button>Choose this Plan</Button>
+      </div>
 
-    </div>
+    </CardSpotlight>
   )
 }
 
