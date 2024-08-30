060481aca6d6966342dbca2ee82910dd990b62a1
nameerakhter <akhtarnameer@gmail.com>
2024-08-30 20:42:32 +0530

    bug fixes for review cards

--- src/components/Reviews.tsx
+++ src/components/Reviews.tsx
@@ -3,8 +3,14 @@ import customerReviews from '../customerReviews.json'
 
 const Reviews = () => {
   return (
-    <div className="p-[3rem]">
+    <div className="p-[3rem] flex flex-col items-center justify-center">
+      <div className="flex items-center jsutify-center gap-3 text-4xl font-extrabold pb-[1rem]">
+        <h1>What Our</h1>
+        <h1 className="text-[#c60811]">Customers Say</h1>
+      </div>
+      <p className="text-nowrap pb-[1rem]">At This Part you can See Few Of The Many Positive reviews Of Our Customers.</p>
       <InfiniteMovingCards items={customerReviews} direction="right" speed="slow" pauseOnHover={false} />
+      <p className="text-nowrap pt-[1rem]">So what are you waiting for Start your Journey today!</p>
 
 
     </div>
--- src/components/ui/infinite-moving-cards.tsx
+++ src/components/ui/infinite-moving-cards.tsx
@@ -87,7 +87,7 @@ export const InfiniteMovingCards = ({
       >
         {items.map((item, idx) => (
           <li
-            className="w-[350px] max-w-full relative rounded-2xl border border-b-0 flex-shrink-0 border-[#c60811] px-8 py-6 md:w-[450px]"
+            className="w-[350px] max-w-full relative rounded-2xl border  flex-shrink-0 border-[#c60811] px-8 py-6 md:w-[450px]"
             style={{
               background:
                 "#1D1D1D",
@@ -104,10 +104,10 @@ export const InfiniteMovingCards = ({
               </span>
               <div className="relative z-20 mt-6 flex flex-row items-center">
                 <span className="flex flex-col gap-1">
-                  <span className=" text-sm leading-[1.6] text-gray-400 font-normal">
+                  <span className=" text-sm leading-[1.6] text-gray-100 font-normal">
                     {item.name}
                   </span>
-                  <span className=" text-sm leading-[1.6] text-gray-400 font-normal">
+                  <span className=" text-sm leading-[1.6] text-gray-100 font-normal">
                     {item.title}
                   </span>
                 </span>
