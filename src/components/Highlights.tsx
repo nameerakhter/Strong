4d4d8cbdf9a50ecfe1cc29ee12d1ce3193260ca3
nameerakhter <akhtarnameer@gmail.com>
2024-09-04 21:44:33 +0530

    feat: NumberTicker from magicUi

--- src/components/Highlights.tsx
+++ src/components/Highlights.tsx
@@ -2,15 +2,15 @@ import React from 'react'
 import Labels from './ui/Labels'
 
 const Highlights = () => {
-    return (
-        <div className='p-[1rem] flex items-center justify-center md:px-[.2rem]'>
-            <section className='w-1/4 flex flex-col items-center justify-center '><Labels colorT1='text-[#d70a14]' text1='96%' text2='Client Satisfaction' text3='Our members love their results and experience' /></section>
-            <section className='w-1/4 flex flex-col items-center justify-center '><Labels colorT1='text-[#c94a16]' text1='+5' text2='Years of experience' text3='Trust in our proven track record of transforming' /></section>
-            <section className='w-1/4 flex flex-col items-center justify-center '><Labels colorT1='text-[#d70a14]' text1='+800' text2='Active Members' text3='Join our thriving fitness community' /></section>
-            <section className='w-1/4 flex flex-col items-center justify-center '><Labels colorT1='text-[#c94a16]' text1='24/7' text2='Support Available' text3='Expert assistance whenever you need it' /></section>
+  return (
+    <div className='p-[1rem] flex items-center justify-center md:px-[.2rem]'>
+      <section className='w-1/4 flex flex-col items-center justify-center '><Labels colorT1='text-[#d70a14]' text1={96} text2='Client Satisfaction' text3='Our members love their results and experience' /></section>
+      <section className='w-1/4 flex flex-col items-center justify-center '><Labels colorT1='text-[#c94a16]' text1={+5} text2='Years of experience' text3='Trust in our proven track record of transforming' /></section>
+      <section className='w-1/4 flex flex-col items-center justify-center '><Labels colorT1='text-[#d70a14]' text1={+800} text2='Active Members' text3='Join our thriving fitness community' /></section>
+      <section className='w-1/4 flex flex-col items-center justify-center '><Labels colorT1='text-[#c94a16]' text1={24 / 7} text2='Support Available' text3='Expert assistance whenever you need it' /></section>
 
-        </div>
-    )
+    </div>
+  )
 }
 
 export default Highlights
